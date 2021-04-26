790bea7b76c5047c3544c336c45a0750c527954b
Daniel Rosel <danalves24@outlook.com>
2021-04-27 00:44:05 +0200

    cleaner structure

---
+++ NaturalLanguageProcessingLibrary/Core/TextCleanup/Cleaner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalLanguageProcessingLibrary.Core.TextCleanup
+{
+    public interface Cleaner
+    {
+        public string cleanText(String text);
+    }
+}
---
+++ NaturalLanguageProcessingLibrary/Core/TextCleanup/StopWordCleaner.cs
@@ -0,0 +1,22 @@
+﻿using NaturalLanguageProcessingLibrary.Core.TextCleanup.Shemas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace NaturalLanguageProcessingLibrary.Core.TextCleanup
+{
+    public class StopWordCleaner : Cleaner
+    {
+        string Cleaner.cleanText(string text)
+        {
+            String data = File.ReadAllText("../../../Dependencies/stopwords.json");
+            StopWords stopWords = JsonSerializer.Deserialize<StopWords>(data);
+            return data;
+        }
+    }
+}
